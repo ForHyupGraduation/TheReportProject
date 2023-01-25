96af65782b4d17cca3a67a17c928c9ba9d5692d8
ordinary-person-life <122017503+ordinary-person-life@users.noreply.github.com>
2023-01-25 21:34:25 +0900

    add small DB

--- Front/src/components/List/CompanyList/CompanyList.js
+++ Front/src/components/List/CompanyList/CompanyList.js
@@ -2,23 +2,35 @@ import React, { useState } from "react";
 import { Accordion } from "react-bootstrap";
 import CompanyListElement from "./CompanyListElement";
 
+
 const CompanyList = ({ companies }) => {
+  console.log(companies);
   return(
-    <Accordion>
-      {companies.map((company, index) => {
-        return (
-          <CompanyListElement 
-            key={index}
-            interestPoint={company.interestPoint}
-            growthPoint={company.growthPoint}
-            companyName={company.companyName}
-            eventKey={index}
-          />
-        )
-      })}
-    </Accordion>
+    <div>
+      
+    </div>
   )
-};
+}
+
+
+//
+//const CompanyList = ({ companies }) => {
+//  return(
+//    <Accordion>
+//      {companies.map((company, index) => {
+//        return (
+//          <CompanyListElement 
+//            key={index}
+//            interestPoint={company.interestPoint}
+//            growthPoint={company.growthPoint}
+//            companyName={company.companyName}
+//            eventKey={index}
+//          />
+//        )
+//      })}
+//    </Accordion>
+//  )
+//};
 
 export default CompanyList;
 
--- Front/src/pages/Home.js
+++ Front/src/pages/Home.js
@@ -75,40 +75,43 @@ function Home() {
             />
           </div>
         </div>
+
+        <div>
+        <table className="table caption-top">
+          <caption>List of users</caption>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">COMPANY</th>
+              <th scope="col">INTEREST</th>
+              <th scope="col">GROWTH</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
       </>
     );
   }
 }
 
 export default Home;
-
-const Doughnut = styled.div`
-  display: flex;
-  background-color: gray;
-  width: 50%;
-  height: 400px;
-  justify-content: space-around;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const Content = styled.div`
-  display: flex;
-`;
-
-const CompanyInfo = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const Line = styled.div`
-  margin: 20px 0;
-  width: 100%;
-  height: 300px;
-  background-color: gray;
-`;
-
-const ChartName = styled.div`
-  display: flex;
-  justify-content: center;
-`;
