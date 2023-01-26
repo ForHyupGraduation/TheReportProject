b4f5255d42dbf47105db36851adfd85bb2d46886
Picka <marmong@naver.com>
2023-01-26 21:21:08 +0900

    sol_crash

--- Front/src/components/List/CompanyList/CompanyList.js
+++ Front/src/components/List/CompanyList/CompanyList.js
@@ -2,37 +2,7 @@ import React, { useState } from "react";
 import { Accordion } from "react-bootstrap";
 import CompanyListElement from "./CompanyListElement";
 
-
 const CompanyList = ({ companies }) => {
-<<<<<<< HEAD
-  console.log(companies);
-  return(
-    <div>
-      
-    </div>
-  )
-}
-
-
-//
-//const CompanyList = ({ companies }) => {
-//  return(
-//    <Accordion>
-//      {companies.map((company, index) => {
-//        return (
-//          <CompanyListElement 
-//            key={index}
-//            interestPoint={company.interestPoint}
-//            growthPoint={company.growthPoint}
-//            companyName={company.companyName}
-//            eventKey={index}
-//          />
-//        )
-//      })}
-//    </Accordion>
-//  )
-//};
-=======
   return (
     <table class="table table-striped">
       <thead>
@@ -60,6 +30,5 @@ const CompanyList = ({ companies }) => {
     </table>
   );
 };
->>>>>>> d1ea0b84848efa4240a734498521149f2796a2e6
 
 export default CompanyList;
